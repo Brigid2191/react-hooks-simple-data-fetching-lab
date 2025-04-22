a569bc28cb384b20d04797b14323d099baaacae0
Brigid Michelle Syondie <brigidmichelle02@gmail.com>
2025-04-22 09:45:22 +0300

    Finishd labs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+// index.js
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Make sure App.js is in the same folder or adjust the path accordingly
 
-import "./index.css";
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
